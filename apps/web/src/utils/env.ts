aeea550566e08d69a7586236ac0147baebd0f735
cooder <cooder@111965.xyz>
2024-03-02 07:23:19 +0800

    fix(client): env for dev

--- apps/web/src/utils/env.ts
+++ apps/web/src/utils/env.ts
@@ -1,3 +1,5 @@
-export const serverOriginUrl =
-  window.__WEWE_RSS_SERVER_ORIGIN_URL__ ||
-  import.meta.env.VITE_SERVER_ORIGIN_URL;
+export const isProd = import.meta.env.PROD;
+
+export const serverOriginUrl = isProd
+  ? window.__WEWE_RSS_SERVER_ORIGIN_URL__
+  : import.meta.env.VITE_SERVER_ORIGIN_URL;
