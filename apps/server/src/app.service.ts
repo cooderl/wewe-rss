b4364bff35deab7fa7b21d174a9734850c981f93
cooder <cooder@111965.xyz>
2024-03-01 16:55:34 +0800

    feat: add server render web

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigurationType } from './configuration';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getFeedConfig() {
+    return this.configService.get<ConfigurationType['feed']>('feed')!;
   }
 }
--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -6,7 +6,5 @@ interface ImportMetaEnv {
 }
 
 interface Window {
-  __WEWE_RSS_RUNTIME_CONFIG__?: {
-    readonly VITE_SERVER_ORIGIN_URL: string;
-  };
+  __WEWE_RSS_SERVER_ORIGIN_URL__?: string;
 }
