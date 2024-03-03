e2e9cb5db8584a4868daef03f270c7889214589b
cooder <cooder@111965.xyz>
2024-03-03 10:32:16 +0800

    refactor(server): add default SERVER_ORIGIN_URL

--- apps/server/src/configuration.ts
+++ apps/server/src/configuration.ts
@@ -9,7 +9,7 @@ const configuration = () => {
 
   const authCode = process.env.AUTH_CODE;
   const platformUrl = process.env.PLATFORM_URL || 'https://weread.111965.xyz';
-  const originUrl = process.env.SERVER_ORIGIN_URL;
+  const originUrl = process.env.SERVER_ORIGIN_URL || '';
 
   const feedMode = process.env.FEED_MODE as 'fulltext' | '';
 
